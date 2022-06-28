7d3c20d490c3e934dbeaa9cce6d5cc4680d16215
gyu0714 <lagyu99@gmail.com>
2022-06-29 02:35:49 +0900

    RESTsAPI document

--- erp-project-backend/src/main/java/com/java/springboot/SpringbootReactBackendApplication.java
+++ erp-project-backend/src/main/java/com/java/springboot/SpringbootReactBackendApplication.java
@@ -1,10 +1,8 @@
 package com.java.springboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.java.springboot.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class SpringbootReactBackendApplication {
